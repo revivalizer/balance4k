7f6c5e80fcb675eab01e3ea5e939637a6deafd2d
Ralph Brorsen <ralphbrorsen@hey.com>
2025-09-11 14:49:52 +0200

    Remove japanese comments from template files, add template credit shaders 1307

--- balance.gfx.glsl
+++ balance.gfx.glsl
@@ -1,20 +1,6 @@
-﻿#version 430	/* version ディレクティブが必要な場合は必ず 1 行目に書くこと */
-/* Copyright (C) 2020 Yosshin(@yosshin4004) */
+﻿#version 430
+// CREDIT: Template from https://github.com/yosshin4004/minimal_gl/blob/master/examples/01_basic_exe_exportable.gfx.glsl, says "Copyright (C) 2020 Yosshin(@yosshin4004)"
 
-/*
-	超基本サンプル。
-
-	エクスポートされた実行ファイル上では、time はサウンド再生位置を示す
-	waveOutPosition から計算で求める必要がある。また、resolution は uniform
-	変数として与えられなくなるので、解像度を知るにはプリプロセッサ定義の
-	SCREEN_XRESO, SCREEN_YRESO を参照する必要がある。
-
-	シェーダコードが現在エディットモードで使用されているかエクスポート
-	された実行ファイルから利用されているかは、プリプロセッサ定義
-	EXPORT_EXECUTABLE の有無で判別可能である。
-
-	煩雑になるが、シェーダ導入部で以下のような定型的な処理を記述することになる。
-*/
 layout(location = 0) uniform int waveOutPosition;
 #if defined(EXPORT_EXECUTABLE)
 	vec2 resolution = {SCREEN_XRESO, SCREEN_YRESO};
--- balance.snd.glsl
+++ balance.snd.glsl
@@ -1,23 +1,8 @@
-﻿#version 430	/* version ディレクティブが必要な場合は必ず 1 行目に書くこと */
-/* Copyright (C) 2020 Yosshin(@yosshin4004) */
-
-/*
-	サウンド出力サンプルコード。
-*/
+﻿#version 430
+// CREDIT: Template from https://github.com/yosshin4004/minimal_gl/blob/master/examples/04_sound_output.snd.glsl, says "Copyright (C) 2020 Yosshin(@yosshin4004)"
 
 layout(location = 0) uniform int waveOutPosition;
 #if defined(EXPORT_EXECUTABLE)
-	/*
-		shader minifier が compute シェーダに対応していない問題を回避するためのハック。
-		以下の記述はシェーダコードとしては正しくないが、shader minifier に認識され
-		minify が適用されたのち、work_around_begin: 以降のコードに置換される。
-		%s は、shader minifier によるリネームが適用されたあとのシンボル名に
-		置き換えらえる。
-
-		buffer にはレイアウト名を付ける必要がある。ここでは、レイアウト名 = ssbo と
-		している。レイアウト名は shader minifier が生成する他のシンボルと衝突しては
-		いけないので、極端に短い名前を付けることは推奨されない。
-	*/
 	#pragma work_around_begin:layout(std430,binding=0)buffer ssbo{vec2 %s[];};layout(local_size_x=1)in;
 	vec2 waveOutSamples[];
 	#pragma work_around_end
