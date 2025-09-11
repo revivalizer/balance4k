61a0eb2abd940ebde669790da512f4350736dc02
Ralph Brorsen <ralphbrorsen@hey.com>
2025-09-11 14:21:41 +0200

    First example 1306

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Using
 * minimal gl: https://github.com/yosshin4004/minimal_gl/ version 2025_05_28
 * Crinkler: https://github.com/runestubbe/Crinkler version 2.3
 * Shader Minifier: https://github.com/laurentlb/shader-minifier/ version 1.5.1
+
+# Resolution
+According to TRSAC rules: https://trsac.dk/realtime-compos/
+"You must supply at least a 1920 x 1080 and 1280 x 720 resolution version"
---
+++ balance.snd.glsl
@@ -0,0 +1,36 @@
+﻿#version 430	/* version ディレクティブが必要な場合は必ず 1 行目に書くこと */
+/* Copyright (C) 2020 Yosshin(@yosshin4004) */
+
+/*
+	サウンド出力サンプルコード。
+*/
+
+layout(location = 0) uniform int waveOutPosition;
+#if defined(EXPORT_EXECUTABLE)
+	/*
+		shader minifier が compute シェーダに対応していない問題を回避するためのハック。
+		以下の記述はシェーダコードとしては正しくないが、shader minifier に認識され
+		minify が適用されたのち、work_around_begin: 以降のコードに置換される。
+		%s は、shader minifier によるリネームが適用されたあとのシンボル名に
+		置き換えらえる。
+
+		buffer にはレイアウト名を付ける必要がある。ここでは、レイアウト名 = ssbo と
+		している。レイアウト名は shader minifier が生成する他のシンボルと衝突しては
+		いけないので、極端に短い名前を付けることは推奨されない。
+	*/
+	#pragma work_around_begin:layout(std430,binding=0)buffer ssbo{vec2 %s[];};layout(local_size_x=1)in;
+	vec2 waveOutSamples[];
+	#pragma work_around_end
+#else
+	layout(std430, binding = 0) buffer SoundOutput{ vec2 waveOutSamples[]; };
+	layout(local_size_x = 1) in;
+#endif
+
+
+#define NUM_SAMPLES_PER_SEC 48000.
+void main(){
+	int offset = int(gl_GlobalInvocationID.x) + waveOutPosition;
+	float sec = float(offset) / NUM_SAMPLES_PER_SEC;
+	waveOutSamples[offset] = sin(vec2(sec * 440 * 6.2831)) * exp(-sec);
+}
+
